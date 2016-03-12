1b71a1bb63f292ea8c735c5105a41083577ee18b
John Lauer as ChiliPeppr on Github <jlauer@chilipeppr.com>
2016-03-12 18:21:37 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- widget.js
+++ widget.js
@@ -102,6 +102,7 @@ cpdefine("inline:com-chilipeppr-widget-recvtext", ["chilipeppr_ready"], function
             
             //this.injectDiv();
             
+            this.btnSetup();
             this.forkSetup();
             this.subscribeSetup();
             
@@ -635,6 +636,138 @@ cpdefine("inline:com-chilipeppr-widget-recvtext", ["chilipeppr_ready"], function
         onStop: function(param1, param2) {
             console.log("zipwhip texting. got onStop signal. param1:", param1, "param2:", param2);
             $('#com-chilipeppr-widget-recvtext .panel-title-status').text("onStop");
+        },
+        /**
+         * Call this method from init to setup all the buttons when this widget
+         * is first loaded. This basically attaches click events to your 
+         * buttons. It also turns on all the bootstrap popovers by scanning
+         * the entire DOM of the widget.
+         */
+        btnSetup: function() {
+
+            // Chevron hide/show body
+            var that = this;
+            $('#' + this.id + ' .hidebody').click(function(evt) {
+                console.log("hide/unhide body");
+                if ($('#' + that.id + ' .panel-body').hasClass('hidden')) {
+                    // it's hidden, unhide
+                    that.showBody(evt);
+                }
+                else {
+                    // hide
+                    that.hideBody(evt);
+                }
+            });
+
+            // Ask bootstrap to scan all the buttons in the widget to turn
+            // on popover menus
+            $('#' + this.id + ' .btn').popover({
+                delay: 1000,
+                animation: true,
+                placement: "auto",
+                trigger: "hover",
+                container: 'body'
+            });
+
+        },
+        /**
+         * User options are available in this property for reference by your
+         * methods. If any change is made on these options, please call
+         * saveOptionsLocalStorage()
+         */
+        options: null,
+        /**
+         * Call this method on init to setup the UI by reading the user's
+         * stored settings from localStorage and then adjust the UI to reflect
+         * what the user wants.
+         */
+        setupUiFromLocalStorage: function() {
+
+            // Read vals from localStorage. Make sure to use a unique
+            // key specific to this widget so as not to overwrite other
+            // widgets' options. By using this.id as the prefix of the
+            // key we're safe that this will be unique.
+
+            // Feel free to add your own keys inside the options 
+            // object for your own items
+
+            var options = localStorage.getItem(this.id + '-options');
+
+            if (options) {
+                options = $.parseJSON(options);
+                console.log("just evaled options: ", options);
+            }
+            else {
+                options = {
+                    showBody: true,
+                    tabShowing: 1,
+                    customParam1: null,
+                    customParam2: 1.0
+                };
+            }
+
+            this.options = options;
+            console.log("options:", options);
+
+            // show/hide body
+            if (options.showBody) {
+                this.showBody();
+            }
+            else {
+                this.hideBody();
+            }
+
+        },
+        /**
+         * When a user changes a value that is stored as an option setting, you
+         * should call this method immediately so that on next load the value
+         * is correctly set.
+         */
+        saveOptionsLocalStorage: function() {
+            // You can add your own values to this.options to store them
+            // along with some of the normal stuff like showBody
+            var options = this.options;
+
+            var optionsStr = JSON.stringify(options);
+            console.log("saving options:", options, "json.stringify:", optionsStr);
+            // store settings to localStorage
+            localStorage.setItem(this.id + '-options', optionsStr);
+        },
+        /**
+         * Show the body of the panel.
+         * @param {jquery_event} evt - If you pass the event parameter in, we 
+         * know it was clicked by the user and thus we store it for the next 
+         * load so we can reset the user's preference. If you don't pass this 
+         * value in we don't store the preference because it was likely code 
+         * that sent in the param.
+         */
+        showBody: function(evt) {
+            $('#' + this.id + ' .panel-body').removeClass('hidden');
+            $('#' + this.id + ' .panel-footer').removeClass('hidden');
+            $('#' + this.id + ' .hidebody span').addClass('glyphicon-chevron-up');
+            $('#' + this.id + ' .hidebody span').removeClass('glyphicon-chevron-down');
+            if (!(evt == null)) {
+                this.options.showBody = true;
+                this.saveOptionsLocalStorage();
+            }
+        },
+        /**
+         * Hide the body of the panel.
+         * @param {jquery_event} evt - If you pass the event parameter in, we 
+         * know it was clicked by the user and thus we store it for the next 
+         * load so we can reset the user's preference. If you don't pass this 
+         * value in we don't store the preference because it was likely code 
+         * that sent in the param.
+         */
+        hideBody: function(evt) {
+            $('#' + this.id + ' .panel-body').addClass('hidden');
+            $('#' + this.id + ' .panel-footer').addClass('hidden');
+            $('#' + this.id + ' .hidebody span').removeClass('glyphicon-chevron-up');
+            $('#' + this.id + ' .hidebody span').addClass('glyphicon-chevron-down');
+            if (!(evt == null)) {
+                this.options.showBody = false;
+                this.saveOptionsLocalStorage();
+            }
         },
         forkSetup: function () {
             var topCssSelector = '#' + this.id; //com-chilipeppr-widget-tinyg';
